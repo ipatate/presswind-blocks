66d58f3416cbe1bf247e606df11613800f1c35e0
Patrick Faramaz <ipatate@users.noreply.github.com>
2025-03-18 15:08:39 +0100

    bump version to 1.0.1

--- presswind-blocks.php
+++ presswind-blocks.php
@@ -1,56 +1,13 @@
-<?php
-
 /**
- * Plugin Name:       presswind-blocks
- * Description:       Block for starter theme Presswind/Goodmotion.
- * Version:           1.0.0
- * Requires at least: 6.7
- * Requires PHP:      8.0
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       presswind-blocks
- * GitHub Plugin URI: ipatate/presswind-blocks
+ * Plugin Name:       Presswind Blocks
+ * Description:       blocks for use with starter theme Goodmotion
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:          1.0.1
+ * Author:           Patrick Faramaz
+ * License:          GPL-2.0-or-later
+ * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:      presswind-blocks
  *
- * @package CreateBlock
+ * @package          create-block
  */
-
-if (! defined('ABSPATH')) {
-	exit; // Exit if accessed directly.
-}
-
-
-require_once dirname(__FILE__) . '/src/carousel/render/carousel.php';
-require_once dirname(__FILE__) . '/src/carousel/variation.php';
-
-
-function create_block_presswindblocs_block_init()
-{
-	register_block_type(__DIR__ . '/build/burger-btn');
-	register_block_type(__DIR__ . '/build/wrapper-navigation');
-	register_block_type(__DIR__ . '/build/lang-switcher');
-	register_block_type(__DIR__ . '/build/mega-menu');
-}
-add_action('init', 'create_block_presswindblocs_block_init');
-
-
-
-/**
- * Adds a custom template part area for mega menus to the list of template part areas.
- *
- * @param array $areas Existing array of template part areas.
- * @return array Modified array of template part areas including the new "Menu" area.
- */
-function presswind_mega_menu_template_part_areas( array $areas ) {
-	$areas[] = array(
-		'area'        => 'menu',
-		'area_tag'    => 'div',
-		'description' => __( 'Menu templates are used to create sections of a mega menu.', 'presswind-blocks' ),
-		'icon'        => 'menu',
-		'label'       => __( 'Menu', 'presswind-blocks' ),
-	);
-
-  return $areas;
-}
-
-add_filter( 'default_wp_template_part_areas', 'presswind_mega_menu_template_part_areas' );
