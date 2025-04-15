40b69ca10ae916852564fc334e31f27ed14608ce
ipatate <faramazpat@gmail.com>
2025-04-15 12:19:53 +0200

    Bump version to 1.0.3

--- presswind-blocks.php
+++ presswind-blocks.php
@@ -4,7 +4,7 @@
  * Description:       blocks for use with starter theme Goodmotion
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:          1.0.1
+ * Version:          1.0.3
  * Author:           Patrick Faramaz
  * License:          GPL-2.0-or-later
  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
