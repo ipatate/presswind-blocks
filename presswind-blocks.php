3a13ab5cc32e5eb680bbb355850afe3317e3f8db
ipatate <faramazpat@gmail.com>
2025-04-15 12:22:39 +0200

    feat: Add GitHub Plugin URI header
    Allows plugin updates directly from GitHub via compatible tools.

--- presswind-blocks.php
+++ presswind-blocks.php
@@ -9,6 +9,7 @@
  * License:          GPL-2.0-or-later
  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:      presswind-blocks
+ * GitHub Plugin URI: ipatate/presswind-blocks
  *
  * @package Presswind
  */
